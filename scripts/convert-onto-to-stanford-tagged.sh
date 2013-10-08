30ae1eaf279276aaf0e383c09240242d11c07e56
Emma Strubell <strubell@cs.umass.edu>
2013-10-08 18:46:58 -0400

    more scripts

--- scripts/convert-onto-to-stanford-tagged.sh
+++ scripts/convert-onto-to-stanford-tagged.sh
@@ -4,5 +4,5 @@ SUFF=".stag"
 
 for file in $1/*.pmd
 do
-    awk -vORS=" " '{print $2"_"$5};' $file | sed 's/ _ /'$'\n/g' > $file$SUFF
+    awk -vORS=" " '{print $2"_"$5};' $file | sed 's/ _ /\n/g' > $file$SUFF
 done
