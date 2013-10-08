06ff61e5c0176094ac1403c9aece1cafd0e71d6c
Emma Strubell <strubell@cs.umass.edu>
2013-10-08 16:19:41 -0400

    NER

--- src/main/scala/edu/umass/cs/iesl/strubell/BenchFactorie.scala
+++ src/main/scala/edu/umass/cs/iesl/strubell/BenchFactorie.scala
@@ -28,32 +28,35 @@ object BenchFactorie {
     }
 
     println("Loading file lists...")
-    var testFileList = getFileListFromDir(testDir, "pmd")
-    var trainFileList = getFileListFromDir(trainDir, "pmd")
+//    var testFileList = getFileListFromDir(testDir, "pmd")
+//    var trainFileList = getFileListFromDir(trainDir, "pmd")
     //var testFileList = getFileListFromDir(testDir, "dep.2")
 
 
-    println("Loading documents...")
-    val testDocs = testFileList.map(LoadOntonotes5.fromFilename(_).head)
-    val trainDocs = trainFileList.map(LoadOntonotes5.fromFilename(_).head)
+//    println("Loading documents...")
+//    val testDocs = testFileList.map(LoadOntonotes5.fromFilename(_).head)
+//    val trainDocs = trainFileList.map(LoadOntonotes5.fromFilename(_).head)
+//
+//    println("Getting sentences from documents...")
+//    val testSentences = testDocs.map(_.sentences).flatten
+//    val trainSentences = trainDocs.map(_.sentences).flatten
 
-    println("Getting sentences from documents...")
-    val testSentences = testDocs.map(_.sentences).flatten
-    val trainSentences = trainDocs.map(_.sentences).flatten
-
-    var numRuns = 10
+    var numRuns = 1
     var taggerFileName = "ontonotes-vanilla.tagger"
     
     //trainPOSTagger(trainSentences, testSentences, true, taggerFileName)
     
-    val serializedTagger = pos.ForwardPOS-Ontonotes //new pos.POS1
-    //println("Deserializing POS tagger...")
-    //serializedTagger.deserialize(new File(taggerFileName))
+//    val serializedTagger = pos.ForwardPOS-Ontonotes //new pos.POS1
+//    //println("Deserializing POS tagger...")
+//    //serializedTagger.deserialize(new File(taggerFileName))
+//    
+//    //testPOSTagger(serializedTagger, testSentences, numRuns)
     
-    testPOSTagger(serializedTagger, testSentences, numRuns)
+    val conllTestFile = "/Users/strubell/Documents/research/data/conll2003/eng.testa"
+    //val conllTestFile = "/Users/strubell/Documents/research/data/conll2003/testb"
     
-    //val namedent = ner.NER3
-    //testNER(namedent, testDocs, numRuns)
+    val conllTestDoc = Seq(LoadConll2003.fromFilename(conllTestFile).head)
+    testNER(conllTestDoc, numRuns)
     
     //val dp = trainDP(trainSentences, testSentences)
     //val dp = parse.DepParser1Ontonotes
@@ -78,7 +81,7 @@ object BenchFactorie {
     }
   }
   
-  def trainDP(trainSentences: Seq[Sentence], testSentences: Seq[Sentence], serialize: Boolean = false, saveName: String = DEFAULT_TAGGER_SAVENAME): parse.DepParser1 = {
+  def trainDP(trainSentences: Seq[Sentence], testSentences: Seq[Sentence], serialize: Boolean = false, saveName: String = DEFAULT_TAGGER_SAVENAME): parse.TransitionParser = {
     val dp = new parse.TransitionParser
     println("Training TransitionParser model...")
     implicit val rng = new scala.util.Random(RANDOM_SEED)
@@ -108,21 +111,21 @@ object BenchFactorie {
 	println("Token accuracy: " + tokAccuracy)
   }
 
-  def testNER(namedent: ner.StackedNER[BilouConllNerLabel], testDocs: Seq[Document], numRuns: Integer) = {
-
+  def testNER(testDocs: Seq[Document], numRuns: Integer) = {
+	val namedent = ner.StackedConllNER
     println("Testing named entity recognition...")
     var sentSpeedTotal = 0.0
     var tokSpeedTotal = 0.0
     for (i <- 1 to numRuns) {
       implicit val rng = new scala.util.Random(RANDOM_SEED)
-//      namedent.printEvaluation(testDocs)
-//      var (sentPerSec, tokPerSec) = namedent.detailedAccuracy(testDocs)
-//      sentSpeedTotal += sentPerSec
-//      tokSpeedTotal += tokPerSec
-//      //println("accuracy: " + accuracy)
-//      println("Sentences/sec: " + sentPerSec)
-//      println("Tokens/sec: " + tokPerSec)
-//      println()
+      namedent.printEvaluation(testDocs)
+      var (sentPerSec, tokPerSec) = namedent.detailedAccuracy(testDocs)
+      sentSpeedTotal = sentSpeedTotal + sentPerSec
+      tokSpeedTotal = tokSpeedTotal + tokPerSec
+      //println("accuracy: " + accuracy)
+      println("Sentences/sec: " + sentPerSec)
+      println("Tokens/sec: " + tokPerSec)
+      println()
     }
     println("Average speed over " + numRuns + " trials: " + sentSpeedTotal/numRuns + " sents/sec " + tokSpeedTotal + "toks/sec")
   }
