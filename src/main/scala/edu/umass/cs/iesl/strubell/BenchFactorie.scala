ebebfc80e5d8ccae90ca0817822373b196c86675
Emma Strubell <strubell@cs.umass.edu>
2013-10-08 20:36:33 -0400

    fixes for POS, NER

--- src/main/scala/edu/umass/cs/iesl/strubell/BenchFactorie.scala
+++ src/main/scala/edu/umass/cs/iesl/strubell/BenchFactorie.scala
@@ -8,55 +8,80 @@ import cc.factorie.app.nlp.load._
 import scala.io._
 import cc.factorie.app.nlp.ner._
 
-object BenchFactorie {
+object BenchPOS {
 
   var RANDOM_SEED = 0
   var DEFAULT_TAGGER_SAVENAME = "NO-NAME.tagger"
   
   def main(args: Array[String]) {
 
-    var trainDir = ""
-    var testDir = ""
-    if (args.size > 0) {
-      testDir = args(0)
-    } else {
-      println("Not going to bother running with no test data. Exiting")
-      System.exit(0)
-    }
-    if (args.size > 1) {
-      trainDir = args(1)
-    }
+    var whichTagger = args(0)
+    var testDir = args(1)
+    var trainDir = args(2)
+    
+    println("Loading tagger...")
+//    val tagger:pos.ForwardPOSTagger = {
+//	    if(whichTagger == "onto"){
+//	      pos.ForwardPOSTaggerOntonotes
+//	    }
+//	    else if(whichTagger == "wsj"){
+//	      pos.ForwardPOSTagger
+//	    }
+//    }
+  
+    val tagger = pos.ForwardPOSTagger
 
     println("Loading file lists...")
-//    var testFileList = getFileListFromDir(testDir, "pmd")
-//    var trainFileList = getFileListFromDir(trainDir, "pmd")
+    var testFileList = getFileListFromDir(testDir, "pmd")
+    var trainFileList = getFileListFromDir(trainDir, "pmd")
     //var testFileList = getFileListFromDir(testDir, "dep.2")
 
 
-//    println("Loading documents...")
-//    val testDocs = testFileList.map(LoadOntonotes5.fromFilename(_).head)
-//    val trainDocs = trainFileList.map(LoadOntonotes5.fromFilename(_).head)
-//
-//    println("Getting sentences from documents...")
-//    val testSentences = testDocs.map(_.sentences).flatten
-//    val trainSentences = trainDocs.map(_.sentences).flatten
+    println("Loading documents...")
+    val testDocs = testFileList.map(LoadOntonotes5.fromFilename(_).head)
+    val trainDocs = trainFileList.map(LoadOntonotes5.fromFilename(_).head)
 
-    var numRuns = 1
+    println("Getting sentences from documents...")
+    val testSentences = testDocs.map(_.sentences).flatten
+    val trainSentences = trainDocs.map(_.sentences).flatten
+
+    
     var taggerFileName = "ontonotes-vanilla.tagger"
+    if(whichTagger == None)
+    	trainPOSTagger(trainSentences, testSentences, true, taggerFileName)
     
-    //trainPOSTagger(trainSentences, testSentences, true, taggerFileName)
+    //println("Deserializing POS tagger...")
+    //serializedTagger.deserialize(new File(taggerFileName))
     
-//    val serializedTagger = pos.ForwardPOS-Ontonotes //new pos.POS1
-//    //println("Deserializing POS tagger...")
-//    //serializedTagger.deserialize(new File(taggerFileName))
-//    
-//    //testPOSTagger(serializedTagger, testSentences, numRuns)
+    var numRuns = 10
     
-    val conllTestFile = "/Users/strubell/Documents/research/data/conll2003/eng.testa"
+    var results = for(i <- 1 to numRuns) yield {tagger.detailedAccuracy(testSentences)}
+    
+    var tokSpeed = results.map(_._3).sum/numRuns
+    var tokAccuracy = results.map(_._1).sum/numRuns
+    var sentAccuracy = results.map(_._2).sum/numRuns
+    
+//    var tokSpeedTotal:Double = 0.0
+//	var tokAccuracy:Double = 0.0
+//	var sentAccuracy:Double = 0.0
+//    for (i <- 1 to numRuns) {
+//      implicit val rng = new scala.util.Random(RANDOM_SEED)
+//      var(tokenAccuracy, sentenceAccuracy, speed, tokens) = tagger.detailedAccuracy(testSentences)
+//      tokSpeedTotal += speed
+//      if(i == numRuns){
+//    	tokAccuracy = tokenAccuracy
+//    	sentAccuracy = sentenceAccuracy
+//      }
+//    }
+	println("Average speed over " + numRuns + " trials: " + tokSpeed)
+	println("Sentence accuracy: " + sentAccuracy)
+	println("Token accuracy: " + tokAccuracy)
+    
+    //val conllTestFile = "/Users/strubell/Documents/research/data/conll2003/eng.testa"
     //val conllTestFile = "/Users/strubell/Documents/research/data/conll2003/testb"
     
-    val conllTestDoc = Seq(LoadConll2003.fromFilename(conllTestFile).head)
-    testNER(conllTestDoc, numRuns)
+    //val conllTestDoc = Seq(LoadConll2003.fromFilename(conllTestFile).head)
+    //testNER(conllTestDoc, numRuns)
     
     //val dp = trainDP(trainSentences, testSentences)
     //val dp = parse.DepParser1Ontonotes
@@ -93,43 +118,6 @@ object BenchFactorie {
     dp
   }
 
-  def testPOSTagger(tagger: pos.ForwardPOSTagger, testSentences: Seq[Sentence], numRuns: Integer) = {
-	var tokSpeedTotal = 0.0
-	var tokAccuracy = 0.0
-	var sentAccuracy = 0.0
-    for (i <- 1 to numRuns) {
-      implicit val rng = new scala.util.Random(RANDOM_SEED)
-      var(tokenAccuracy, sentenceAccuracy, speed, tokens) = tagger.detailedAccuracy(testSentences)
-      tokSpeedTotal += speed
-      if(i == numRuns){
-    	tokAccuracy = tokenAccuracy
-    	sentAccuracy = sentenceAccuracy
-      }
-    }
-	println("Average speed over " + numRuns + " trials: " + tokSpeedTotal/numRuns)
-	println("Sentence accuracy: " + sentAccuracy)
-	println("Token accuracy: " + tokAccuracy)
-  }
-
-  def testNER(testDocs: Seq[Document], numRuns: Integer) = {
-	val namedent = ner.StackedConllNER
-    println("Testing named entity recognition...")
-    var sentSpeedTotal = 0.0
-    var tokSpeedTotal = 0.0
-    for (i <- 1 to numRuns) {
-      implicit val rng = new scala.util.Random(RANDOM_SEED)
-      namedent.printEvaluation(testDocs)
-      var (sentPerSec, tokPerSec) = namedent.detailedAccuracy(testDocs)
-      sentSpeedTotal = sentSpeedTotal + sentPerSec
-      tokSpeedTotal = tokSpeedTotal + tokPerSec
-      //println("accuracy: " + accuracy)
-      println("Sentences/sec: " + sentPerSec)
-      println("Tokens/sec: " + tokPerSec)
-      println()
-    }
-    println("Average speed over " + numRuns + " trials: " + sentSpeedTotal/numRuns + " sents/sec " + tokSpeedTotal + "toks/sec")
-  }
-
   def testDP(dp: parse.TransitionParser, testSentences: Seq[Sentence], numRuns: Integer) = {
 
     println("Testing dependency parser...")
@@ -142,8 +130,8 @@ object BenchFactorie {
 //      println("Tokens/sec: " + tokPerSec)
     }
   }
-
-  /**
+  
+    /**
    * Returns a list of the file names of files with the given ending under the given directory
    */
   def getFileListFromDir(fileName: String, ending: String=""): Seq[String] = {
@@ -166,5 +154,41 @@ object BenchFactorie {
     val t1 = System.nanoTime()
     println("Elapsed time: " + (t1 - t0) + "ns")
     result
+  }
+}
+
+object BenchNER{
+  
+	var RANDOM_SEED = 0
+  
+   def main(args: Array[String]) {
+
+    val conllTestFile = args(0)
+    var numRuns = 10
+    val conllTestDoc = Seq(LoadConll2003.fromFilename(conllTestFile).head)
+    testNER(conllTestDoc, numRuns)
+    
+    //val dp = trainDP(trainSentences, testSentences)
+    //val dp = parse.DepParser1Ontonotes
+    //testDP(dp, testSentences, numRuns) 
+  }
+  
+    def testNER(testDocs: Seq[Document], numRuns: Integer) = {
+	val namedent = ner.StackedConllNER
+    println("Testing named entity recognition...")
+    var sentSpeedTotal = 0.0
+    var tokSpeedTotal = 0.0
+    for (i <- 1 to numRuns) {
+      implicit val rng = new scala.util.Random(RANDOM_SEED)
+      namedent.printEvaluation(testDocs)
+      var (sentPerSec, tokPerSec) = namedent.detailedAccuracy(testDocs)
+      sentSpeedTotal = sentSpeedTotal + sentPerSec
+      tokSpeedTotal = tokSpeedTotal + tokPerSec
+      //println("accuracy: " + accuracy)
+      println("Sentences/sec: " + sentPerSec)
+      println("Tokens/sec: " + tokPerSec)
+      println()
+    }
+    println("Average speed over " + numRuns + " trials: " + sentSpeedTotal/numRuns + " sents/sec " + tokSpeedTotal + "toks/sec")
   }
 }
